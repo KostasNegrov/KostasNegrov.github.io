be8ab6fb917e307c2e1d151982c41959c72b3ce9
KostasNegrov <kostas29@mail.ru>
2020-02-26 20:50:45 +0300

    новый проект

---
+++ dovaina/gulpfile.js
@@ -0,0 +1,18 @@
+global.$ = {
+  gulp: require('gulp'),
+  gp: require('gulp-load-plugins')(),  /* Этот плагин дает возможность подключать плагины которые начинаются gulp- */
+  browserSync: require('browser-sync').create(),  /* Подключаем браузер синк */
+  del: require('del'),   /*  */
+  imageminJpegRecompress: require('imagemin-jpeg-recompress'),
+  pngquant: require('imagemin-pngquant'),
+  path: {
+    config: require('./gulp/config'), /* подключаем файл config.js */  
+    // wow: './app/libs/wow/wow.js', /* подключаем библиотеку */
+        
+    js: './app/js/**/*.js',               /* подключаем файл js */
+  }
+};
+
+$.path.config.forEach(function (path) {   /* подключаем каждый таск */
+  require(path)();
+});
